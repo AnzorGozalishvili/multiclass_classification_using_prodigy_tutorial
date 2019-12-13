d0821eb312aab2d7890a1e787140640710b7aa6b
anz2 <anzor.gozalishvili2014@ens.tsu.edu.ge>
2019-12-14 02:57:10 +0400

    Add toxic dataset, notebooks, sample terms and patterns files, some helper module to convert some phrases to patterns. Add README for describing workflow. Add empty requirements which will be updated later. Add install script which installs requirements and downloads spacy english model. Add gitignore for exclude some unnecessary files.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # multiclass_classification_using_prodigy_tutorial
 In this tutorial I will show an example of multiclass text classification using prodigy and spacy. 
+
+
+# Preprocess Data
+See data preprocessing in ``notebooks/data_preparation.ipynb``
+
+# Explore texts of each class and create list of terms
+Files in directory: ``prodigy/terms`` contain some list of terms for each class (with `.text` extensions)
+
+# Generate spaCy style patterns using listed terms to use boostraping technique in labeling Prodigy Labeling Tool
+run following module to generate `.jsonl` files for each term
+```bash
+python -m prodigy.helpers.phrase_to_pattern
+``` 
+
+# Initialize database in prodigy
+init default (Sqlite) database in current directory
+```bash
+
+```
---
+++ prodigy/helpers/phrase_to_pattern.py
@@ -0,0 +1,80 @@
+import json
+import spacy
+import os
+
+
+def terms_and_classes(directory):
+    for item in os.listdir(directory):
+        if item.endswith('.txt'):
+            yield item, item[:-4].upper()
+
+
+def read_terms(path):
+    with (open(path)) as file:
+        return file.readlines()
+
+
+def patterns_to_jsonl(data, path):
+    with open(path, 'w') as file:
+        for item in data:
+            file.write(json.dumps(item) + '\n')
+
+
+def generate_lower_pattern(text, nlp):
+    doc = nlp(text)
+    tokens = [item.text for item in doc]
+
+    pattern = []
+    for token in tokens:
+        pattern.append({'LOWER': token})
+
+    return pattern
+
+
+def generate_lemma_pattern(text, nlp):
+    doc = nlp(text)
+    tokens = [item.lemma_ for item in doc]
+
+    pattern = []
+    for token in tokens:
+        pattern.append({'LEMMA': token})
+
+    return pattern
+
+
+def generate_pos_pattern(text, nlp):
+    doc = nlp(text)
+    tokens = [item.pos_ for item in doc]
+
+    pattern = []
+    for token in tokens:
+        pattern.append({'POS': token})
+
+    return pattern
+
+
+def generate_all_patterns(text, nlp):
+    all_patterns = [
+        generate_lower_pattern(text, nlp),
+        generate_lemma_pattern(text, nlp),
+        generate_pos_pattern(text, nlp)
+    ]
+    return all_patterns
+
+
+if __name__ == '__main__':
+    spacy_model = spacy.load('en_core_web_sm')
+
+    terms_dir = 'prodigy/terms'
+    terms_out_dir = 'prodigy/terms'
+
+    for terms_file, class_name in terms_and_classes(terms_dir):
+        generated_patterns = []
+        terms = read_terms(os.path.join(terms_dir, terms_file))
+        for term in terms:
+            term_patterns = generate_all_patterns(term, spacy_model)
+
+            for term_pattern in term_patterns:
+                generated_patterns.append({"label": class_name, "pattern": term_pattern})
+
+        patterns_to_jsonl(generated_patterns, os.path.join(terms_out_dir, f'{class_name}.jsonl'))
